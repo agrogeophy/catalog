fba8cfa25a981815dea615ed7022fe95dc95d9fb
BenjiM <benjaminmary.etu@gmail.com>
2020-08-20 18:02:40 +0200

    minor changes

---
+++ dbb/connect_db.php
@@ -0,0 +1,13 @@
+<?php 
+
+$uname = "YourUsername";
+$pass = "YourPassword";
+$dsn = 'mysql:dbname=agrigeophy_zhao;host=127.0.0.1';
+
+try {
+    $conn = new PDO($dsn, $uname, $pass);
+} catch (PDOException $e) {
+    echo 'Connexion échouée : ' . $e->getMessage();
+}
+
+?>
